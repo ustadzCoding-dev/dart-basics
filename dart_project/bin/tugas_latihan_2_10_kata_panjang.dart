fb25303b0c59f7486152b86627a8754edbd1a54b
Afrizal Ilza Munadhif <ilzamunadhif@gmail.com>
2026-03-08 22:58:04 +0700

    add: A program that iterates through a list of words and prints each word along with its length

---
+++ dart_project/bin/tugas_latihan_2_10_kata_panjang.dart
@@ -0,0 +1,7 @@
+void main() {
+  final daftarKata = ['Dart', 'Flutter', 'Mobile', 'Programming', 'Kotlin'];
+
+  for (final kata in daftarKata) {
+    print('Kata: "$kata", Panjang: ${kata.length}');
+  }
+}
